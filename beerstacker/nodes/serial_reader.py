a6126540f79ba24d54c8ee930c27a01930e341fd
TheUncivil <46045333+TheUncivil@users.noreply.github.com>
2019-12-16 10:05:58 +0100

    Add files via upload
    Added the first version of the ROS software

---
+++ beerstacker/nodes/serial_reader.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+#The data provided by the BeerStacker should be read as follows:
+#	a: slowly go backwards
+#	b: stay still
+#	c: slowly go forwards
+#	d: drive normally
+
+def talker():
+ while not rospy.is_shutdown():
+   data= ser.read(1) 
+   rospy.loginfo(data)
+   pub.publish(String(data))
+   rospy.sleep(0.2)
+
+
+if __name__ == '__main__':
+  try:
+    rospy.init_node('BeerStacker_serial')
+    pub = rospy.Publisher('BeerStacker_data', String)
+    talker()
+  except rospy.ROSInterruptException:
+    pass
